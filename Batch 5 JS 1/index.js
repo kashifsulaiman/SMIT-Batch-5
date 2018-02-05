77fa61816ef280dd760926e7d784c752721c50c1
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2018-02-05 11:35:17 +0500

    JS 1

---
+++ Batch 5 JS 1/index.js
@@ -0,0 +1,19 @@
+var name = "5416513nnhjfytr76rf65468";
+
+
+alert(name); //parenthesis
+/*
+alert(name); //parenthesis
+alert('Kashif'); //parenthesis
+alert('Kashif'); //parenthesis
+alert('Kashif'); //parenthesis
+*/
+
+name = "Mark"; //declaration + definition
+
+alert(name);
+
+var kolachi; //declaration
+console.log('before defining:  ', kolachi)
+kolachi = "do darya";
+console.log('after defining:  ', kolachi)
