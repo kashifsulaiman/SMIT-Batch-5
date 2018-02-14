a925cf9d6520fbb1d19cdf1e1238cf7639bed1e1
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2018-02-14 14:26:28 +0500

    js3 and js4

---
+++ Batch 5 JS 3/js3.js
@@ -0,0 +1,49 @@
+number >= secondNumber
+var city = "Karachi";
+var phone = true;
+var name = "Kashif"
+if(name == "Kashif" && city == "Karachi" && phone == true){
+	alert('Hamara banda agya');
+}
+
+true + true = true
+false + true = false
+true + false = false
+false + false = false
+
+if(name == "Kashif" && city == "Lahore" && phone == true || city == "Karachi"){
+	alert('Hamara banda agya');
+}
+
+if(name == "Kashif" && city == "Karachi"){
+	alert('Hamara banda agya');
+} else if(name == "Wasif" && city == "Lahore") {
+	alert('wasif milgya')
+}
+
+if(84 && 76 && 56){
+
+alert()
+}
+
+if(0 && 76 && 56){
+	alert('jhj');
+}
+
+true && false || true && 98 || 13
+' ' || 13
+(true || false) && false
+var list = ["kashif", "javed", "wasif", 6, false];
+list[2]
+
+list.length
+
+list.push("hashir", "dawod")
+list.pop()
+list.unshift("kashif", 78)
+list.shift()
+list.splice(1, 1)
+list.splice(1, 1, 111, true, "hello")
+if(list.splice(1, 0, "yaar")){
+	alert("kolachi")
+}
