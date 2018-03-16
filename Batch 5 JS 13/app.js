5ec6d343276686cd91664f4643b86adc368077cc
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2018-03-16 11:59:04 +0500

    JS 13

---
+++ Batch 5 JS 13/app.js
@@ -0,0 +1,8 @@
+debugger;
+document.childNodes[0].childNodes[1].childNodes[1].childNodes[0].innerHTML;
+
+
+function doDarya() {
+    var allParagraphs = document.getElementsByTagName("p");
+    allParagraphs[2].style.fontSize = '50px';
+}
