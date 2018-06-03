59270caa1cf59c14edeb979c371fe92942be1612
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2018-06-03 10:39:13 +0500

    PWA implemented on 3rd June 2018

---
+++ PWA - Sun - 6 June 2018/js/app.js
@@ -0,0 +1,5 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('./service-worker.js')
+        .then(function() { console.log('Service Worker Registered'); });
+}
---
+++ PWA - Sun - 6 June 2018/service-worker.js
@@ -0,0 +1,37 @@
+var cacheName = 'test me';
+var filesToCache = ['/', '/index.html', '/css/web.css', '/css/mobile.css', '/images/image2.jpg', '/js/app.js'];
+
+self.addEventListener('install', function(e) {
+    self.skipWaiting();
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
